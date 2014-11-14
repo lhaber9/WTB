28af54597d999caf42fe2508a13432b6d04f350d
Lucas Haber <lhaber@carecloud.com>
2014-11-14 09:53:49 -0500

    Add some stuff

--- Walking The Border/Walking The Border/MainViewController.h
+++ Walking The Border/Walking The Border/MainViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainViewController.h
 //  Walking The Border
 //
 //  Created by Lucas Haber on 11/14/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MainViewController : UIViewController
 
 @end
-
